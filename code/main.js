3a764a0bd93da771b054c16d9ad2683b77d7e54b
Dennis <dennistrangg@gmail.com>
2023-06-19 12:21:31 -0700

    added 'assets folder' and modified css layout

---
+++ code/main.js
@@ -0,0 +1,22 @@
+// constants
+
+
+
+
+// cached element
+const initStart = document.querySelector('h1');
+
+
+
+
+// event listeners
+initStart.addEventListener('click', initGame);
+
+
+
+
+
+// functions
+// initGame(){
+
+// };
