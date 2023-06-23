baed6989ef6b41721193ddaadb64f8962735701b
Dennis <dennistrangg@gmail.com>
2023-06-22 19:55:47 -0700

    cleaned up CSS and add function on letter divs

--- code/main.js
+++ code/main.js
@@ -1,73 +1,7 @@
 // constants
-
-
-
-
-// cached element
-const btnPlay = document.querySelector('#playBtn');
-const letters = document.getElementById('letter-ctr');
-const option = document.querySelector('.optionCtr');
-const playAgain = document.querySelector('.reset');
-const rdmWordBank = document.querySelector('.optionRandom');
-const rdmSpaceWordBank = document.querySelector('.optionSpace');
-
-
-
-
-// event listeners
-btnPlay.addEventListener('click', hidePlay);
-option.addEventListener('click', renderGame);
-playAgain.addEventListener('click', initGame);
-
-rdmSpaceWordBank.addEventListener('click', function(){
-    let spaceWord = wordBank.space[Math.floor(Math.random() * wordBank.space.length)];
-    console.log(spaceWord);
-});
-rdmWordBank.addEventListener('click', function(){
-    let rdmWord = wordBank.randomWords[Math.floor(Math.random() * wordBank.randomWords.length)];
-    console.log(rdmWord);
-});
-
-
-
-
-
-
-// functions
-function initGame(){
-// hidePlay();   
-renderGame();
-};
-
-function hidePlay() {
-btnPlay.style.visibility = 'hidden';
-option.style.visibility = 'visible';
-}
-
-function renderGame(){
-    option.style.visibility = 'hidden';
-    console.log('on click, this will  be hidden');
-    wordGenList();
-    displayOptions();
-
-}
-
-function wordGenList(){
-
-};
-
-const displayOptions = () => {
-    option.innerHTML += `<h3>Please Select An Option</h3>`;
-    let buttonCon = document.createElement("div");
-    for (let value in option) {
-      buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
-    }
-    option.appendChild(buttonCon);
-  };
-
 // two options to choose from for wordBank
 const wordBank ={
-space:   [
+    space:[
     "Asteroid",
     "Big Bang",
     "Black hole",
@@ -121,7 +55,7 @@ space:   [
     "Lunar module"
   ],
 
-randomWords: [
+randomWords:[
     "apple",
     "banana",
     "carrot",
@@ -176,3 +110,115 @@ randomWords: [
     "zeppelin"
 ]
 };
+let guessesLeft = 8;
+
+//generate random word from object arrays
+let spaceWord = wordBank.space[Math.floor(Math.random() * wordBank.space.length)];
+let rdmWord = wordBank.randomWords[Math.floor(Math.random() * wordBank.randomWords.length)];
+
+
+
+
+// cached element
+const btnPlay = document.querySelector('#playBtn');
+const btnLetters = document.querySelectorAll('.letter-ctr div');
+const option = document.querySelector('.optionCtr');
+const playAgainBtn = document.querySelector('.reset');
+const rdmWordBank = document.querySelector('.optionRandom');
+const rdmSpaceWordBank = document.querySelector('.optionSpace');
+const backgroundImg = document.querySelector('.movingPic');
+const hiddenWord = document.querySelector('.hiddenWord');
+
+
+
+// event listeners
+btnPlay.addEventListener('click', function(){
+    option.style.visibility = 'visible';
+    btnPlay.style.visibility = 'hidden';
+});
+option.addEventListener('click', function(){
+    option.style.visibility = 'hidden';
+});
+
+playAgainBtn.addEventListener('click', resetGame);
+
+rdmSpaceWordBank.addEventListener('click', function(){
+    backgroundImg.src = "../assets/error0.png";
+    spaceWord;
+    // let spaceWord = wordBank.space[Math.floor(Math.random() * wordBank.space.length)];
+    console.log(spaceWord);
+    hiddenWord.innerHTML = '';
+    for(let i = 0; i < spaceWord.length; i++){
+        let letter = document.createElement('div');
+        letter.innerText += '_';
+        letter.classList.add('letter');
+        hiddenWord.append(letter);
+    };
+    playerGuessLetters();
+    // checkWin();
+});
+rdmWordBank.addEventListener('click', function(){
+    backgroundImg.src = "../assets/error0.png";
+    rdmWord;
+    // let rdmWord = wordBank.randomWords[Math.floor(Math.random() * wordBank.randomWords.length)];
+    // console.log(rdmWord);
+    hiddenWord.innerHTML = '';
+    for(let i = 0; i < rdmWord.length; i++){
+        let letter = document.createElement('div');
+        letter.innerText = '_';
+        hiddenWord.append(letter);
+    };
+    playerGuessLetters();
+    // checkWin();
+});
+
+// functions
+initializeGame();
+function initializeGame(){
+    btnPlay.style.visibility = 'visible';
+    backgroundImg.src = "../assets/error08.png";
+};
+
+function resetGame(){
+    initializeGame();
+    
+}
+
+function playerGuessLetters() {
+    btnLetters.forEach(function(letter) {
+      letter.addEventListener('click', function () {
+        const selectedLetter = letter.innerText;
+        console.log('Selected letter:', selectedLetter);
+        checkWin();
+    });
+    });
+  }
+
+// [...btnLetters].forEach(function (div){
+    //     div.addEventListener('click', function(){
+        //     console.log(div.innerHTML); 
+        //     });
+        // }); 
+        
+        function checkWin(){
+            const hiddenLetters = document.querySelectorAll('.letter');
+            const revealLetters = [...hiddenLetters].map(function(letter){
+                return letter.innerText;
+            })
+            // .join('');
+            
+            // if (!isCorrectGuess) {
+            //   guessesLeft--;
+            // }
+            
+            if(revealLetters === rdmWord || revealLetters === spaceWord){
+                console.log('I guess you want me to stay...');
+            } else if (guessesLeft === 0) {
+                console.log('Game over! Maybe Next Time');
+            } else {
+                guessesLeft--;
+        console.log(`Incorrect guess! ${guessesLeft} guesses left`);
+    }
+}
+
+
